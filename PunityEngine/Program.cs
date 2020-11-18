46ce918d93c90004aac9c2d1d7a6099d3e5f236a
voro282 <43708321+voro282@users.noreply.github.com>
2020-11-18 15:09:45 +0100

    yes

--- PunityEngine/Program.cs
+++ PunityEngine/Program.cs
@@ -16,12 +16,15 @@ namespace PunityEngine
 
             Texture2D logo = Raylib.LoadTextureFromImage(Raylib.LoadImage("EngineAssets/icon.png"));
 
+            Raylib.SetTargetFPS(60);
+            
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.BLACK);
                 Raylib.DrawTexture(logo, Raylib.GetScreenWidth()/2-90, Raylib.GetScreenHeight()/2-160, Color.WHITE);
-                Raylib.DrawText("Punity Engine", Raylib.GetScreenWidth()/2-110, Raylib.GetScreenHeight()/2+10, 30, Color.WHITE);         
+                Raylib.DrawText("Punity Engine", Raylib.GetScreenWidth()/2-110, Raylib.GetScreenHeight()/2+10, 30, Color.WHITE);    
+                Raylib.DrawFPS(0, 50);     
                 Raylib.EndDrawing();
             }
 
